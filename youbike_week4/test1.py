52ab90cc867cde7bd6f9f2dc24a113c02e373241
TsaiFucheng <a0908345017@gmail.com>
2025-10-02 11:53:27 +0800

    20251002

--- youbike_week4/test1.py
+++ youbike_week4/test1.py
@@ -1,5 +1,13 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
 data = pd.read_csv("./data/youbike.csv")
 # print(data.head())
 # print(data.info())
-print(data.isnull().sum())
+# print(data.isnull().sum())
+# print(data.duplicated().sum())
+
+sns.boxenplot(data['available_rent_bikes'])
+plt.show()
